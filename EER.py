40b078dbea5bd03198db66b9060515f51ef3d0aa
mehdipakt <57838392+mehdipakt@users.noreply.github.com>
2019-12-02 16:04:54 +0100

    Add files via upload

---
+++ EER.py
@@ -0,0 +1,15 @@
+from sklearn import metrics
+import numpy as np
+def evaluateEER(user_scores, imposter_scores):
+    labels = [0]*len(user_scores) + [1]*len(imposter_scores)
+    fpr, tpr, thresholds = metrics.roc_curve(labels, user_scores + imposter_scores)
+    missrates = 1 - tpr
+    farates = fpr
+    dists = missrates - farates
+    idx1 = np.argmin(dists[dists >= 0])
+    idx2 = np.argmax(dists[dists <= 0])
+    x = [missrates[idx1], farates[idx1]]
+    y = [missrates[idx2], farates[idx2]]
+    a = ( x[0] - x[1] ) / ( y[1] - x[1] - y[0] + x[0] )
+    eer = x[0] + a * ( y[0] - x[0] )
+    return eer
